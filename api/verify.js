2c136909d52871963d6bd652bd38f6314b054ae0
fairytaledesign41-ism <fairytaledesign41@gmail.com>
2026-06-15 05:59:08 +0200

    Update verify.js

--- api/verify.js
+++ api/verify.js
@@ -1,4 +1,7 @@
+const jwt = require('jsonwebtoken');
+const https = require('https');
+const crypto = require('crypto');
+
 module.exports = async function handler(req, res) {
-  // هذا الكود سيعيد ردًا بسيطًا جدًا ليخبرنا أن السيرفر يعمل
-  res.status(200).json({ status: "Vercel is alive and connected!" });
+  res.status(200).json({ status: "Libraries loaded successfully" });
 };
